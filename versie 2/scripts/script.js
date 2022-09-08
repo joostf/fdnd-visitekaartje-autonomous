c8d19c33295e09e4ca10ca2abbd19d441fb9f203
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2022-09-08 12:02:00 +0200

    versie 2 visitekaartje

---
+++ versie 2/scripts/script.js
@@ -0,0 +1,32 @@
+//const expandButton = document.querySelector('header button')
+//expandButton.addEventListener('click', expand)
+
+//function expand () {
+//document.body.classList.toggle('expand')
+//}
+
+
+
+
+const openCardButton = document.querySelectorAll('btn1')
+const openCardButton = document.querySelectorAll('btn2')
+const openCardButton = document.querySelectorAll('btn3"')
+
+openCardButton.forEach(button => {
+  button.addEventListener('click', () => {
+    const button = document.querySelector(card_1)
+  })
+})
+
+openCardButton.forEach(button => {
+  button.addEventListener('click', () => {
+    const button = document.querySelector(card_2)
+  })
+})
+
+
+openCardButton.forEach(button => {
+  button.addEventListener('click', () => {
+    const button = document.querySelector(card_3)
+  })
+})
